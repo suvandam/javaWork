8266f6f02d41c713f33d722a961fb6c8650845b3
suryaverma <suryaverma@192.168.0.30>
2017-09-19 01:11:30 -0700

    ArrayList All

---
+++ JavaCollectionEx/src/com/collection/arraylist/ArrayListSorting.java
@@ -0,0 +1,34 @@
+package com.collection.arraylist;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class ArrayListSorting {
+
+	public static void main(String[] args) {
+
+		ArrayList<Employee> empList=new ArrayList<>();
+		
+		empList.add(new Employee(102,"Ram"));
+		empList.add(new Employee(101,"John"));
+		empList.add(new Employee(103,"Mike"));
+		
+		Collections.sort(empList);
+		
+		for(Iterator<Employee> itr=empList.iterator();itr.hasNext();) {
+			Employee e=itr.next();
+			System.out.println(e.getEmpId()+" - "+e.getEmpName());
+		}
+		
+		empList.sort(Employee.empNameComp);
+		//or this way
+		//Collections.sort(empList, Employee.empNameComp);
+		
+		for(Iterator<Employee> itr=empList.iterator();itr.hasNext();) {
+			Employee e=itr.next();
+			System.out.println(e.getEmpId()+" - "+e.getEmpName());
+		}
+	}
+
+}
---
+++ JavaCollectionEx/src/com/collection/arraylist/Employee.java
@@ -0,0 +1,48 @@
+package com.collection.arraylist;
+
+import java.util.Comparator;
+
+public class Employee implements Comparable<Employee>{
+
+	private int empId;
+	private String empName;
+	
+	public Employee(int id,String name) {
+		this.empId=id;
+		this.empName=name;
+	}
+	
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	@Override
+	public int compareTo(Employee empObj) {
+		// ASC
+		return this.empId-empObj.getEmpId();
+		
+		//DESC
+		//return empObj.getEmpId()-this.empId;
+		
+	}
+	
+	public static Comparator<Employee> empNameComp=new Comparator<Employee>() {
+
+		@Override
+		public int compare(Employee o1, Employee o2) {
+			String name1=o1.getEmpName().toUpperCase();
+			String name2=o2.getEmpName().toUpperCase();
+			return name1.compareTo(name2);
+		}
+	};
+	
+	
+}

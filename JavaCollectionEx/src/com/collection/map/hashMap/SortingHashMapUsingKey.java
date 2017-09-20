e5835f312a88af7e6fc0e6cba88d2acb38708271
suryaverma <suryaverma@192.168.0.30>
2017-09-20 00:14:45 -0700

    Tets

---
+++ JavaCollectionEx/src/com/collection/map/hashMap/SortingHashMapUsingKey.java
@@ -0,0 +1,83 @@
+package com.collection.map.hashMap;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class SortingHashMapUsingKey {
+
+	public static void main(String[] args) {
+		Map<String, Integer> olympic2012 = new HashMap<String, Integer>();
+	      
+        olympic2012.put("England", 3);
+        olympic2012.put("USA", 1);
+        olympic2012.put("China", 2);
+        olympic2012.put("Russia", 4);
+        
+        Map<String, Integer> sortedbyKey =sortByKeys(olympic2012);
+        
+        Iterator it = sortedbyKey.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            System.out.println(pair.getKey() + " = " + pair.getValue());
+            it.remove(); // avoids a ConcurrentModificationException
+        }
+
+        Map<String, Integer> sortedbyValue =sortByValues(olympic2012);
+
+        Iterator itr = sortedbyValue.entrySet().iterator();
+        while (itr.hasNext()) {
+            Map.Entry pair = (Map.Entry)itr.next();
+            System.out.println(pair.getKey() + " = " + pair.getValue());
+            itr.remove(); // avoids a ConcurrentModificationException
+        }
+	}
+		
+	/*
+     * Paramterized method to sort Map e.g. HashMap or Hashtable in Java
+     * throw NullPointerException if Map contains null key
+     */
+    public static <K extends Comparable,V extends Comparable> Map<K,V> sortByKeys(Map<K,V> map){
+        List<K> keys = new LinkedList<K>(map.keySet());
+        Collections.sort(keys);
+      
+        //LinkedHashMap will keep the keys in the order they are inserted
+        //which is currently sorted on natural ordering
+        Map<K,V> sortedMap = new LinkedHashMap<K,V>();
+        for(K key: keys){
+            sortedMap.put(key, map.get(key));
+        }
+      
+        return sortedMap;
+    }
+
+	
+    public static <K extends Comparable,V extends Comparable> Map<K,V> sortByValues(Map<K,V> map){
+        List<Map.Entry<K,V>> entries = new LinkedList<Map.Entry<K,V>>(map.entrySet());
+      
+        Collections.sort(entries, new Comparator<Map.Entry<K,V>>() {
+
+            @Override
+            public int compare(Entry<K, V> o1, Entry<K, V> o2) {
+                return o1.getValue().compareTo(o2.getValue());
+            }
+        });
+      
+        //LinkedHashMap will keep the keys in the order they are inserted
+        //which is currently sorted on natural ordering
+        Map<K,V> sortedMap = new LinkedHashMap<K,V>();
+      
+        for(Map.Entry<K,V> entry: entries){
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+      
+        return sortedMap;
+    }
+
+}
